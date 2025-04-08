c67659d589cdb855e8ec726ff6d8c6a4ebeb49fe
Darling91 <nordrum101101@gmail.com>
2025-04-08 19:51:02 +0300

    arrayList and linkedList task

---
+++ src/main/java/collection/MyArrayList.java
@@ -0,0 +1,105 @@
+package collection;
+
+public class MyArrayList<E> {
+    /**
+     * Простейшая реализация ArrayList.
+     */
+        private Object[] elements;
+        private int size;
+
+        public MyArrayList() {
+            elements = new Object[10];
+            size = 0;
+        }
+
+        /**
+         * Добавляет элемент в конец списка.
+         *
+         * @param element элемент, который нужно добавить
+         */
+        public void add(E element) {
+            ensureCapacity();
+            elements[size++] = element;
+        }
+
+        /**
+         * Удаляет элемент по индексу.
+         *
+         * @param index индекс элемента для удаления
+         * @return удаленный элемент
+         */
+        public E remove(int index) {
+            if (index < 0 || index >= size) {
+                throw new IndexOutOfBoundsException();
+            }
+            E removedElement = (E) elements[index];
+            System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+            elements[--size] = null; // Убираем ссылку для сборщика мусора
+            return removedElement;
+        }
+
+        /**
+         * Получает элемент по индексу.
+         *
+         * @param index индекс элемента
+         * @return элемент по указанному индексу
+         */
+        public E get(int index) {
+            if (index < 0 || index >= size) {
+                throw new IndexOutOfBoundsException();
+            }
+            return (E) elements[index];
+        }
+
+        /**
+         * Устанавливает элемент по индексу.
+         *
+         * @param index индекс для установки элемента
+         * @param element новый элемент
+         * @return старый элемент на указанном индексе
+         */
+        public E set(int index, E element) {
+            if (index < 0 || index >= size) {
+                throw new IndexOutOfBoundsException();
+            }
+            E oldElement = (E) elements[index];
+            elements[index] = element;
+            return oldElement;
+        }
+
+        /**
+         * Возвращает подсписок от указанного начального индекса до конечного.
+         *
+         * @param fromIndex начальный индекс
+         * @param toIndex конечный индекс
+         * @return новый MyArrayList, содержащий элементы из указанного диапазона
+         */
+        public MyArrayList<E> subList(int fromIndex, int toIndex) {
+            if (fromIndex < 0 || toIndex > size || fromIndex > toIndex) {
+                throw new IndexOutOfBoundsException();
+            }
+            MyArrayList<E> subList = new MyArrayList<>();
+            for (int i = fromIndex; i < toIndex; i++) {
+                subList.add((E) elements[i]);
+            }
+            return subList;
+        }
+
+        /**
+         * Возвращает количество элементов в списке.
+         *
+         * @return размер списка
+         */
+        public int size() {
+            return size;
+        }
+
+        private void ensureCapacity() {
+            if (size == elements.length) {
+                Object[] newElements = new Object[elements.length * 2];
+                System.arraycopy(elements, 0, newElements, 0, elements.length);
+                elements = newElements;
+            }
+        }
+    }
+
---
+++ src/main/java/collection/MyLinkedList.java
@@ -0,0 +1,132 @@
+package collection;
+
+/**
+ * Простейшая реализация LinkedList.
+ */
+public class MyLinkedList<E> {
+    private class Node {
+        E data;
+        Node next;
+
+        Node(E data) {
+            this.data = data;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    /**
+     * Добавляет элемент в конец списка.
+     *
+     * @param element элемент, который нужно добавить
+     */
+    public void add(E element) {
+        Node newNode = new Node(element);
+        if (tail != null) {
+            tail.next = newNode;
+        } else {
+            head = newNode;
+        }
+        tail = newNode;
+        size++;
+    }
+
+    /**
+     * Удаляет элемент по индексу.
+     *
+     * @param index индекс элемента для удаления
+     * @return удаленный элемент
+     */
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node current = head;
+        Node previous = null;
+        for (int i = 0; i < index; i++) {
+            previous = current;
+            current = current.next;
+        }
+        if (previous != null) {
+            previous.next = current.next;
+        } else {
+            head = current.next;
+        }
+        if (current == tail) {
+            tail = previous;
+        }
+        size--;
+        return current.data;
+    }
+
+    /**
+     * Получает элемент по индексу.
+     *
+     * @param index индекс элемента
+     * @return элемент по указанному индексу
+     */
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    /**
+     * Устанавливает элемент по индексу.
+     *
+     * @param index индекс для установки элемента
+     * @param element новый элемент
+     * @return старый элемент на указанном индексе
+     */
+    public E set(int index, E element) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        E oldElement = current.data;
+        current.data = element;
+        return oldElement;
+    }
+
+    /**
+     * Возвращает подсписок от указанного начального индекса до конечного.
+     *
+     * @param fromIndex начальный индекс
+     * @param toIndex конечный индекс
+     * @return новый MyLinkedList, содержащий элементы из указанного диапазона
+     */
+    public MyLinkedList<E> subList(int fromIndex, int toIndex) {
+        if (fromIndex < 0 || toIndex > size || fromIndex > toIndex) {
+            throw new IndexOutOfBoundsException();
+        }
+        MyLinkedList<E> subList = new MyLinkedList<>();
+        Node current = head;
+        for (int i = 0; i < fromIndex; i++) {
+            current = current.next;
+        }
+        for (int i = fromIndex; i < toIndex; i++) {
+            subList.add(current.data);
+            current = current.next;
+        }
+        return subList;
+    }
+
+    /**
+     * Возвращает количество элементов в списке.
+     *
+     * @return размер списка
+     */
+    public int size() {
+        return size;
+    }
+}
